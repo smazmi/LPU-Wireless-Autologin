a12900e52b41f64787e79a96f5289fb407dc7f7a
smazmi <sadiquemobaraka5@gmail.com>
2024-04-18 02:56:11 +0530

    Rust re-write

--- demo/target/debug/demo.d
+++
@@ -1,1 +0,0 @@
-/home/arno/projects/LPU-Wireless-Autologin/demo/target/debug/demo: /home/arno/projects/LPU-Wireless-Autologin/demo/src/main.rs
--- demo/target/debug/deps/demo-e21ba73a67091321.d
+++
@@ -1,5 +0,0 @@
-/home/arno/projects/LPU-Wireless-Autologin/demo/target/debug/deps/demo-e21ba73a67091321: src/main.rs
-
-/home/arno/projects/LPU-Wireless-Autologin/demo/target/debug/deps/demo-e21ba73a67091321.d: src/main.rs
-
-src/main.rs:
